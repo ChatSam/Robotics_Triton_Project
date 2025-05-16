c8bf2e8d8c48da784606a3cc9144b51746e1cb86
Shripad Deshmukh <deshmukh.shripad.v@gmail.com>
2025-05-15 21:54:43 -0400

    Clean build docker container

--- triton_start.sh
+++ triton_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-sudo docker compose up -d 
+sudo docker compose up -d --force-recreate
 
 sudo docker compose exec triton_noetic bash -c "source devel/setup.bash && roslaunch stingray_camera triton.launch"
